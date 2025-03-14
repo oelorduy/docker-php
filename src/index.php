4ddf1f8cea054e45e129cbcba6d50b3880aaaf3b
oelorduy@gmail.com <oelorduy@gmail.com>
2025-03-13 22:21:24 -0500

    Etiquetas html

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
-<?php 
-echo "Hola Mundo desde Docker"; 
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi página web</title>
+</head>
+<body>
+    <h1><?php echo "Hola, Mundo desde docker" ?></h1>
+</body>
+</html>
